d332c181cb00d6ed08c53cd6bedfb0db62a97d94
Salvatore Iovene <salvatore.iovene@intel.com>
2013-04-05 14:11:01 +0300

    Use different approach to Phantomjs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,10 +74,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['jshint', 'qunit', 'uglify']);
-  grunt.registerTask('test', ['connect', 'qunit']);
-  grunt.registerTask('preview', ['connect', 'watch']);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
